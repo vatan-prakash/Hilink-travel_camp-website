e1d7d1d848b5644ac041964a1d00d4dde791e619
Vatan Prakash <vatanprakash867@gmail.com>
2023-12-26 12:41:31 +0530

    added navbar and navlogo

---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+const Navbar = () => {
+  return (
+    <nav className="border-2 flexBetween max-container padding-container relative z-30 py-5">
+        <Link href="/">
+          <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
+          <ul className='hidden h-full gap-12 lg:flex'>
+             
+          </ul>
+        </Link>
+   
+    </nav>
+  )  
+}
+
+export default Navbar
